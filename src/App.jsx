50e04e3c317a3577d03a74d2fb2e0b0249ef8b3d
jessicavenancio <jessicavenancioti@gmail.com>
2023-03-23 09:48:40 -0300

    Atualização

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Propriedades} from "../src/components/Propriedades/Propriedades";
+
+export function App() {
+  return (
+    <div>
+      <Propriedades 
+      cidade="Alfenas"
+      estado="MG"
+      preco={9}>
+      
+      </Propriedades>
+    </div>
+
+  )
+}
